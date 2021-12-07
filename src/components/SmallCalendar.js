c343bd556e8aaddd6d15d51f3a9c51b2100072f1
JCS <chenone839@gmail.com>
2021-12-07 17:39:36 +0900

    second commit

--- src/components/SmallCalendar.js
+++ src/components/SmallCalendar.js
@@ -5,16 +5,39 @@ import getMonth from '../utils/days';
 const SmallCalendar = () => {
 
     const [currentMonthIndex, setCurrentMonthIndex] = useState(dayjs().month());
-    const [currentmonth, setCurrentMonth] = useState(getMonth());
+    const [currentMonth, setCurrentMonth] = useState(getMonth());
 
     useEffect(() => {
         setCurrentMonth(getMonth(currentMonthIndex));
     }, [currentMonthIndex]);
 
+    const handlePrevMonth = () => {
+        setCurrentMonthIndex(currentMonthIndex - 1);
+
+    };
+
+    const handleNextMonth = () => {
+        setCurrentMonthIndex(currentMonthIndex + 1);
+    };
+
 
     return (
         <div>
-            
+            <header>
+                <p>
+                    {dayjs(new Date(dayjs().year(), currentMonthIndex)).format("YYYY MMMMMM")}
+                </p>
+                <button onClick={handlePrevMonth}>
+                    <span>
+                        왼쪽
+                    </span>
+                </button>
+                <button onClick={handleNextMonth}>
+                    <span>
+                        오른쪽
+                    </span>
+                </button>
+            </header>
         </div>
     );
 };
